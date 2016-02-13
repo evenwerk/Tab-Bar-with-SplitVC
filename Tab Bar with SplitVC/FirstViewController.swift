75865b2959c9ad663939c5c16e699e25cacd999b
evenwerk <evenwerk@gmail.com>
2016-02-13 10:41:21 +0100

    Initial Commit

---
+++ Tab Bar with SplitVC/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tab Bar with SplitVC
+//
+//  Created by Tim Even on 13-02-16.
+//  Copyright © 2016 evenwerk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
